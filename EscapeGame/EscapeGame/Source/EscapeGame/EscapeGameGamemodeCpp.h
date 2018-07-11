e927b0e05c0de30e1b994ac785e97b66be03fd4b
Krasimir Georgiev <krasi2405@gmail.com>
2018-07-11 23:08:16 +0300

    Raycast

---
+++ EscapeGame/EscapeGame/Source/EscapeGame/EscapeGameGamemodeCpp.h
@@ -0,0 +1,20 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "EscapeGameGamemodeCpp.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class ESCAPEGAME_API AEscapeGameGamemodeCpp : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+	
+	
+	
+};
